63a305036850ed2343e9747e5676c62e0e50a88a
unknown <vlad.vtorygin@gmail.com>
2023-03-12 12:48:05 +0200

    Update API.js

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const API_KEY = '32802250-478a62fdefab1f19526f29480';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function getImage(query, page) {
+export async function getImage({ search }, page) {
   try {
     const result = await axios.get(
-      `${BASE_URL}?q=${query.search}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `${BASE_URL}?q=${search}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
     return result.data;
   } catch (error) {
